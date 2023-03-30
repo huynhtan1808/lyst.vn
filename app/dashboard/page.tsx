bd41fcdff05f52e3303bb74f44bea765913ccb3d
huynhtan1808 <tanhuynh1808@gmail.com>
2023-03-30 16:42:57 +0700

    Remove Suspense

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,14 @@
 "use client"
 
 import { useUser } from '@/contexts/AuthContext';
-import { Suspense } from 'react';
-import {CardSkeleton} from '@/components/skeleton/PostCardSkeleton';
-
 
 export default function Dashboard() {
   
   const { user }  = useUser();
   
   return (
-  <Suspense fallback={<CardSkeleton isLoading={true} />}>
     <h1 className='text-2xl font-bold'>
         Hi, {user?.name}
     </h1>
-  </Suspense>
   );
 }
